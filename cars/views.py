36fb5c9a198a2b0b10373d3660b025bb1a6ccb27
Dev-Erasmo-Oliveira <erasmohazzagui021@gmail.com>
2025-05-26 15:42:34 -0300

    Realizado ajuste de quando o usuario nao cadastrar foto ficar uma imagem padrao e nao quebrar o sistema

--- cars/views.py
+++ cars/views.py
@@ -1,22 +1,26 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 
-#view para o usuario pesquisar os carros que ja estão adicionados na loja
+# view para o usuario pesquisar os carros que ja estão adicionados na loja
+
+
 def cars_view(request):
     cars = Car.objects.all()
     search = request.GET.get('search')
 
     if search:
-        cars = Car.objects.filter(model__contains = search)
-
+        cars = Car.objects.filter(model__contains=search)
+    
     return render(
         request,
         'cars.html',
         {'cars': cars}
     )
 
-#view para adicionar o carro que o usuário quer e logo depois redirecionar para a lista de carros novamente
+# view para adicionar o carro que o usuário quer e logo depois redirecionar para a lista de carros novamente
+
+
 def new_car_view(request):
     if request.method == 'POST':
         new_car_form = CarModelForm(request.POST, request.FILES)
