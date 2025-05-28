244378d2ea71109352949945a882bcb857bb1f54
Dev-Erasmo-Oliveira <erasmohazzagui021@gmail.com>
2025-05-28 16:18:06 -0300

    Realizada a criação da Class Based Views e feita a substituição da função cars_view, pela nova classe CarsView, utilizando o CBW na melhor pratica e melhor estruturação da view como uma classe

--- cars/views.py
+++ cars/views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views import View
 
 # view para o usuario pesquisar os carros que ja estão adicionados na loja
 
 
-def cars_view(request):
-    cars = Car.objects.all()
-    search = request.GET.get('search')
+class CarsView(View):
+    def get(self, request):
+        cars = Car.objects.all().order_by('model')
+        search = request.GET.get('search')
 
-    if search:
-        cars = Car.objects.filter(model__contains=search)
-    
-    return render(
-        request,
-        'cars.html',
-        {'cars': cars}
-    )
+        if search:
+            cars = cars.filter(model__icontains=search)
+
+        return render(request, 'cars.html', {'cars': cars})
+
 
 # view para adicionar o carro que o usuário quer e logo depois redirecionar para a lista de carros novamente
 
