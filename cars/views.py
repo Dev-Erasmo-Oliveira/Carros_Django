5ed6fbc0eb6b518f2a92d22ed93be9b9da97ce5d
Dev-Erasmo-Oliveira <erasmohazzagui021@gmail.com>
2025-05-29 13:33:07 -0300

    Feita a reescrita da NewCarsView, utilizando agora a Class Based View ao invés da função, para manter o códiog mais organizado sem muitos IF e ELSE e também separando o metodo GET do metodo POST corretamente

--- cars/views.py
+++ cars/views.py
@@ -19,13 +19,14 @@ class CarsView(View):
 
 # view para adicionar o carro que o usuário quer e logo depois redirecionar para a lista de carros novamente
 
+class NewCarView(View):
+    def get(self, request):
+        new_car_form = CarModelForm()
+        return render(request, 'new_car.html', {'new_car_form': new_car_form})
 
-def new_car_view(request):
-    if request.method == 'POST':
+    def post(self, request):
         new_car_form = CarModelForm(request.POST, request.FILES)
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect('cars_list')
-    else:
-        new_car_form = CarModelForm()
-    return render(request, 'new_car.html', {'new_car_form': new_car_form})
+        return render(request, 'new_car.html', {'new_car_form': new_car_form})
