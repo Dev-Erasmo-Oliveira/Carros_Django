54215890c7c1c51ea5326955cf0c217f3cfd2a65
Dev-Erasmo-Oliveira <erasmohazzagui021@gmail.com>
2025-05-30 16:30:14 -0300

    Feita a reescrita da view NewCarCreateView utilizando a createview com menos linhas de codigo,além de redirecionar o usuario para a lista de carros após ter sido feita a criação

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import CarsListView, NewCarView 
+from cars.views import CarsListView, NewCarCreateView 
 from accounts.views import register_view, login_view, logout_view
 
 urlpatterns = [
@@ -12,7 +12,7 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('register/', register_view, name='register'),
     path('cars/', CarsListView.as_view(), name='cars_list'),
-    path('new_car/', NewCarView.as_view(), name='new_car'),
+    path('new_car/', NewCarCreateView.as_view(), name='new_car'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 """
--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
 # view para o usuario pesquisar os carros que ja estão adicionados na loja
 
@@ -21,14 +19,8 @@ class CarsListView(ListView):
 
 # view para adicionar o carro que o usuário quer e logo depois redirecionar para a lista de carros novamente
 
-class NewCarView(View):
-    def get(self, request):
-        new_car_form = CarModelForm()
-        return render(request, 'new_car.html', {'new_car_form': new_car_form})
-
-    def post(self, request):
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-        return render(request, 'new_car.html', {'new_car_form': new_car_form})
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars/'
