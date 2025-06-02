e2ef9837fd7672827843d9773ccee410d724533f
Dev-Erasmo-Oliveira <erasmohazzagui021@gmail.com>
2025-06-02 14:00:22 -0300

    Adicionado a função de obrigatoriamente estar logado antes de conseguir fazer qualquer alterção no sistema utilizando o @method_decorator, que redireciona o user para a tela de login caso o mesmo esteja deslogado e tentando realizar alterações no sistema

--- cars/views.py
+++ cars/views.py
@@ -1,9 +1,12 @@
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 # view para o usuario pesquisar os carros que ja estão adicionados na loja
+
 
 class CarsListView(ListView):
     model = Car
@@ -17,28 +20,33 @@ class CarsListView(ListView):
             cars = cars.filter(model__icontains=search)
         return cars
 
+#View que irá abrir os detalhes e renderizar o template sobre os detalhes do carro
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
+
 
 # view para adicionar o carro que o usuário quer e logo depois redirecionar para a lista de carros novamente
-
+# metodo decorator irá obrigar o usuário a fazer login antes de conseguir acessar o NEW CAR
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class NewCarCreateView(CreateView):
     model = Car
     form_class = CarModelForm
     template_name = 'new_car.html'
     success_url = '/cars/'
 
-class CarDetailView(DetailView):
-    model = Car
-    template_name = 'car_detail.html'
 
-
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CarUpdateView(UpdateView):
     model = Car
     form_class = CarModelForm
     template_name = 'car_update.html'
 
     def get_success_url(self):
-        return reverse_lazy ('car_detail', kwargs={'pk': self.object.pk})
+        return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
 
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CarDeleteView(DeleteView):
     model = Car
     form_class = CarModelForm
