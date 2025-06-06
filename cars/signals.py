e221e769b459822747a8dcbdca5b6f777e5bacf8
Dev-Erasmo-Oliveira <erasmohazzagui021@gmail.com>
2025-06-06 11:58:33 -0300

    Criada as funções com o RECEIVER que vai escutar e aplicar as alterações toda vez que houver uma mudança no numero de carros em estoque e no valor do estoque também

--- cars/signals.py
+++ cars/signals.py
@@ -1,13 +1,31 @@
-from django.db.models.signals import pre_save, pre_delete, post_save, post_delete 
+from django.db.models.signals import post_save, post_delete
+from django.db.models import Sum
 from django.dispatch import receiver
-from cars.models import Car
-
-@receiver(pre_save, sender=Car)
-def car_pre_save(sender, instance, **kwargs):
-    
-    ...
+from cars.models import Car, CarInventory
 
 
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
-    ...
+    # query para calcular a quantidade de carros e o valor total do estoque
+    cars_count = Car.objects.all().count()
+    cars_value = Car.objects.aggregate(
+        total_value=Sum('value')
+    )['total_value']
+# criação na tabela CarsInventory com os valores tanto dos carros quanto do estoque e armazenar na tabela
+    CarInventory.objects.create(
+        cars_count=cars_count,
+        cars_value=cars_value
+    )
+
+
+@receiver(post_delete, sender=Car)
+def car_post_delete(sender, instance, **kwargs):
+    cars_count = Car.objects.all().count()
+    cars_value = Car.objects.aggregate(
+        total_value=Sum('value')
+    )['total_value']
+
+    CarInventory.objects.create(
+        cars_count=cars_count,
+        cars_value=cars_value
+    )
