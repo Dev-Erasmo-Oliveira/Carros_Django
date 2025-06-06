ee89c945130e574a238ec04d60a1bba5fe1aa986
Dev-Erasmo-Oliveira <erasmohazzagui021@gmail.com>
2025-06-06 13:35:25 -0300

    Realizado a criação de uma função que irá atualiazar todo nosso estoque quando houver alteração, e para não ter a necessidade de alterar em varias partes do codigo quando algum parametro mudar, fiz a criação de uma função isolada e depois chamei essa função dentro do meu receiver, assim eu precisaria mudar somente na função isolada que ja alterava em todos os outros

--- cars/signals.py
+++ cars/signals.py
@@ -4,28 +4,22 @@ from django.dispatch import receiver
 from cars.models import Car, CarInventory
 
 
-@receiver(post_save, sender=Car)
-def car_post_save(sender, instance, **kwargs):
-    # query para calcular a quantidade de carros e o valor total do estoque
+def car_inventory_update():
     cars_count = Car.objects.all().count()
     cars_value = Car.objects.aggregate(
         total_value=Sum('value')
     )['total_value']
-# criação na tabela CarsInventory com os valores tanto dos carros quanto do estoque e armazenar na tabela
+
     CarInventory.objects.create(
         cars_count=cars_count,
         cars_value=cars_value
     )
+
+@receiver(post_save, sender=Car)
+def car_post_save(sender, instance, **kwargs):
+    car_inventory_update()
 
 
 @receiver(post_delete, sender=Car)
 def car_post_delete(sender, instance, **kwargs):
-    cars_count = Car.objects.all().count()
-    cars_value = Car.objects.aggregate(
-        total_value=Sum('value')
-    )['total_value']
-
-    CarInventory.objects.create(
-        cars_count=cars_count,
-        cars_value=cars_value
-    )
+    car_inventory_update()
